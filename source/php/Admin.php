20243d9c85ce4b18d6f82e843fa2bd490f8f7f02
Thor Brink <thor.brink@helsingborg.se>
2023-08-29 11:25:02 +0200

    feat: only allow text blocks in editor (#9)

--- source/php/Admin.php
+++ source/php/Admin.php
@@ -2,12 +2,18 @@
 
 namespace SchoolsManager;
 
+use WP_Block_Editor_Context;
+use WP_Block_Type_Registry;
+
 class Admin
 {
     public function addHooks()
     {
-        add_action('acf/init', array($this, 'addOptionsPage'));
+        add_action('init', array($this, 'addOptionsPage'));
         add_action('after_setup_theme', array($this, 'themeSupport'));
+
+        // Allow only specific block categories.
+        add_filter('allowed_block_types_all', array($this, 'filterAllowedBlockTypes'), 100, 2);
     }
 
     /**
@@ -24,5 +30,17 @@ class Admin
     public function themeSupport()
     {
         add_theme_support('post-thumbnails');
+    }
+
+    public function filterAllowedBlockTypes($allowedBlocks, $blockEditorContext): array
+    {
+        $allBlocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
+
+        // Remove all categories but text.
+        $allowed = array_filter($allBlocks, function ($block) {
+            return $block->category === 'text';
+        });
+
+        return array_keys($allowed);
     }
 }
