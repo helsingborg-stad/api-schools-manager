e55c635852cdb22d7a2266de6f818cd992aaf1fe
Thor Brink <thor.brink@helsingborg.se>
2023-08-30 11:02:27 +0200

    docs: add url for conventional commits (#17)

--- bin/commit-msg.sh
+++ bin/commit-msg.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
+MESSAGE="\n✋ Aborting commit. Your commit message is invalid. Format must follow the Conventional Commits specification.\n👉 See https://www.conventionalcommits.org/en/v1.0.0/ \n" >&2
 
 # Enforce conventional commit message format
 if ! head -1 "$1" | grep -qE "^(feat|fix|chore|docs|test|style|refactor|perf|build|ci|revert)(\(.+?\))?: .{1,}$"; then
-    echo "Aborting commit. Your commit message is invalid. Format must follow the Conventional Commits specification" >&2
-    exit 1
-fi
-if ! head -1 "$1" | grep -qE "^.{1,88}$"; then
-    echo "Aborting commit. Your commit message is too long. Length must follow the Conventional Commits specification" >&2
-    exit 1
+    echo $MESSAGE;
+    exit 1;
 fi
