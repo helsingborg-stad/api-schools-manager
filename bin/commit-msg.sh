35d5c8622a77015093b659c4a4de048d60172bd1
Thor Brink <thor.brink@helsingborg.se>
2023-08-30 10:44:38 +0200

    chore: enforce conventional commits with git hooks (#16)

---
+++ bin/commit-msg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Enforce conventional commit message format
+if ! head -1 "$1" | grep -qE "^(feat|fix|chore|docs|test|style|refactor|perf|build|ci|revert)(\(.+?\))?: .{1,}$"; then
+    echo "Aborting commit. Your commit message is invalid. Format must follow the Conventional Commits specification" >&2
+    exit 1
+fi
+if ! head -1 "$1" | grep -qE "^.{1,88}$"; then
+    echo "Aborting commit. Your commit message is too long. Length must follow the Conventional Commits specification" >&2
+    exit 1
+fi
