f65b48fe8b4d51ab44f2e8e30bd4fd21fae2fd22
Anna Johansson <anna@thehappyhipster.se>
2023-10-10 11:40:13 +0000

    fix: Reimplemented lost meta fields for Person

---
+++ source/php/AcfFields/php/person-details.php
@@ -0,0 +1,67 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_64e6f3b077aa6',
+    'title' => __('Person details', 'api-schools-manager'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_64e6f3b00a717',
+            'label' => __('E-mail', 'api-schools-manager'),
+            'name' => 'e-mail',
+            'aria-label' => '',
+            'type' => 'email',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+        ),
+        1 => array(
+            'key' => 'field_64e6f3e40a718',
+            'label' => __('Phone number', 'api-schools-manager'),
+            'name' => 'phone-number',
+            'aria-label' => '',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'person',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 1,
+));
+}
