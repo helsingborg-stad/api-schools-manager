932cab1254f7fb3462e99eafd4b9e36d219d9a05
Thor Brink <thor.brink@helsingborg.se>
2023-11-09 06:59:00 +0000

    refactor: DefaultValuesSetter

--- source/php/API/DefaultValuesSetter.php
+++ source/php/API/DefaultValuesSetter.php
@@ -2,14 +2,30 @@
 
 namespace SchoolsManager\API;
 
+/**
+ * Sets default values for ACF fields.
+ */
 class DefaultValuesSetter
 {
+    /**
+     * Sets default values for ACF fields.
+     *
+     * @return void
+     */
     public function addHooks()
     {
-        add_filter('acf/load_value', array($this, 'setAcfFieldsDefaultValues'), 10, 3);
+        add_filter('acf/load_value', array($this, 'setDefaultValues'), 10, 3);
     }
 
-    public function setAcfFieldsDefaultValues($value, $postId, $field)
+    /**
+     * Sets default values for ACF fields.
+     *
+     * @param mixed $value The value of the field.
+     * @param int $postId The ID of the post.
+     * @param array $field The field.
+     * @return mixed The default value.
+     */
+    public function setDefaultValues(mixed $value, $postId, array $field): mixed
     {
         if (!$this->isRestRequest()) {
             return $value;
@@ -23,21 +39,29 @@ class DefaultValuesSetter
 
         foreach ($defaultFieldsMap as $fieldName => $defaultFieldNameSuffix) {
             if ($field['name'] === $fieldName) {
-                $value = $this->setDefaultValue($value, $postId, $field, $defaultFieldNameSuffix);
+                $value = $this->getDefaultValue($value, $postId, $field, $defaultFieldNameSuffix);
             }
         }
 
         return $value;
     }
 
-    private function setDefaultValue($value, $postId, $field, string $defaultFieldNameSuffix)
+    /**
+     * Returns the default value for a given field.
+     *
+     * @param mixed $value The value of the field.
+     * @param int $postId The ID of the post.
+     * @param array $field The field.
+     * @param string $defaultFieldNameSuffix The suffix of the default field name.
+     * @return mixed The default value.
+     */
+    private function getDefaultValue(mixed $value, $postId, array $field, string $defaultFieldNameSuffix): mixed
     {
         $isEmpty = $this->getEmptyCheckByField($field);
 
         if ($isEmpty($value)) {
-            $postType         = get_post_type($postId);
-            $defaultFieldName = "default_{$postType}_application_settings_{$defaultFieldNameSuffix}";
-            $defaultValue     = get_field($defaultFieldName, 'option');
+            $defaultFieldName = $this->getDefaultFieldName($postId, $defaultFieldNameSuffix);
+            $defaultValue     = \get_field($defaultFieldName, 'option');
 
             if (!$isEmpty($defaultValue)) {
                 return $defaultValue;
@@ -47,7 +71,26 @@ class DefaultValuesSetter
         return $value;
     }
 
-    private function getEmptyCheckByField(array $field)
+    /**
+     * Returns the name of the default field for a given post type and suffix.
+     *
+     * @param int $postId The ID of the post.
+     * @param string $suffix The suffix of the default field name.
+     * @return string The name of the default field.
+     */
+    private function getDefaultFieldName($postId, string $suffix): string
+    {
+        $postType = get_post_type($postId);
+        return "default_{$postType}_application_settings_{$suffix}";
+    }
+
+    /**
+     * Returns a callable function that checks if a given field is empty.
+     *
+     * @param array $field The field to check.
+     * @return callable The function that checks if the field is empty.
+     */
+    private function getEmptyCheckByField(array $field): callable
     {
         if ($field['type'] === 'link') {
             return fn($value) => empty($value) || (empty($value['url']) && empty($value['title']));
@@ -56,6 +99,11 @@ class DefaultValuesSetter
         return fn($value) => empty($value);
     }
 
+    /**
+     * Check if the current request is a REST request.
+     *
+     * @return bool Returns true if the request is a REST request, false otherwise.
+     */
     private function isRestRequest(): bool
     {
         return function_exists('did_action') && did_action('rest_api_init');
