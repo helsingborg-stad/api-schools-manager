0c77c522d0fccde5ca957b7857b854f5939c7670
GitHub Action <>
2025-09-11 09:17:31 +0000

    build: update package file versions to 0.24.0

--- api-schools-manager.php
+++ api-schools-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Schools Manager
  * Plugin URI:        https://github.com/helsingborg-stad/api-schools-manager
  * Description:       Creates a api that may be used to manage schools
- * Version: 0.23.1
+ * Version: 0.24.0
  * Author:            Thor Brink @ Helsingborg Stad
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
